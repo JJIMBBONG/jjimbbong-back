93b35e1e18bea767520b3ade10128200be9d2972
yongu09 <https://github.com/yongu09>
2025-04-08 17:43:52 +0900

    feat: boardEntity 수정

--- src/main/java/com/ateam/jjimppong_back/common/entity/BoardEntity.java
+++ src/main/java/com/ateam/jjimppong_back/common/entity/BoardEntity.java
@@ -1,5 +1,11 @@
 package com.ateam.jjimppong_back.common.entity;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import com.ateam.jjimppong_back.common.dto.request.board.PatchBoardRequestDto;
+import com.ateam.jjimppong_back.common.dto.request.board.PostBoardRequestDto;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -24,11 +30,34 @@ public class BoardEntity {
     private String boardContent;
     private String boardTitle;
     private String boardAddressCategory;
-    private String boardDetailCatagory;
+    private String boardDetailCategory;
     private String boardWriteDate;
     private Integer boardViewCount;
     private Integer boardScore;
     private String boardAddress;
     private String boardImage;
 
+    public BoardEntity(PostBoardRequestDto dto, String userNickname) {
+        LocalDate now = LocalDate.now();
+        DateTimeFormatter DateTimeFormatter = DateTimeFormatter.ofPattern("YYYY-MM-DD")
+        this.userId = userId;
+        this.userNickname = userNickname;
+        this.boardContent = dto.getBoardContent();
+        this.boardTitle = dto.getBoardTitle();
+        this.boardAddressCategory = dto.getBoardAddressCategory();
+        this.boardDetailCategory = dto.getBoardDetailCategory();
+        this.boardWriteDate = LocalDate.now();
+        this.boardViewCount = 0;
+        this.boardScore = 0;
+        this.boardAddress = dto.getBoardAddress();
+        this.boardImage = dto.getBoardImage();
+    }
+    
+
+    public void patch(PatchBoardRequestDto dto) {
+        this.boardAddressCategory = dto.getBoardAddressCategory();
+        this.boardDetailCategory = dto.getBoardDetailCategory();
+        this.boardTitle = dto.getBoardTitle();
+        this.boardContent = dto.getBoardContent();
+    }
 }
