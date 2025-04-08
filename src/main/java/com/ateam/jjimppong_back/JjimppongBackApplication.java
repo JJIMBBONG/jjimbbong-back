b6076eb4f551e7636c521d1aa4d3b48cc1732c28
SH J <thdghk5429@gmail.com>
2025-04-08 15:04:26 +0900

    0408 first commit

---
+++ src/main/java/com/ateam/jjimppong_back/JjimppongBackApplication.java
@@ -0,0 +1,13 @@
+package com.ateam.jjimppong_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjimppongBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjimppongBackApplication.class, args);
+	}
+
+}
