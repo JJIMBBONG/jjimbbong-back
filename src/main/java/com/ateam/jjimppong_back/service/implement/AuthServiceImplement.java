d28ad80c73745887018cc5653c0a46abed3b0cb2
Artair-kr <hjs6855@naver.com>
2025-04-10 09:39:34 +0900

    feat: 이메일 인증 저장소(Repository), 서비스 추가

--- src/main/java/com/ateam/jjimppong_back/service/implement/AuthServiceImplement.java
+++ src/main/java/com/ateam/jjimppong_back/service/implement/AuthServiceImplement.java
@@ -12,9 +12,17 @@ import com.ateam.jjimppong_back.common.dto.request.auth.IdCheckRequestDto;
 import com.ateam.jjimppong_back.common.dto.request.auth.NicknameCheckRequestDto;
 import com.ateam.jjimppong_back.common.dto.request.auth.SignUpRequestDto;
 import com.ateam.jjimppong_back.common.dto.response.ResponseDto;
+import com.ateam.jjimppong_back.common.entity.EmailAuthEntity;
 import com.ateam.jjimppong_back.common.entity.UserEntity;
+import com.ateam.jjimppong_back.common.util.EmailAuthNumberUtil;
+import com.ateam.jjimppong_back.repository.EmailAuthNumberRepository;
 import com.ateam.jjimppong_back.repository.UserRepository;
 import com.ateam.jjimppong_back.service.AuthService;
+
+import jakarta.mail.MessagingException;
+
+import com.ateam.jjimppong_back.provider.JwtProvider;
+import com.ateam.jjimppong_back.provider.MailProvider;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,7 +30,15 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthServiceImplement implements AuthService{
 
+    // 중복 확인
     private final UserRepository userRepository;
+    // 이메일, 인증번호 확인
+    private final EmailAuthNumberRepository emailAuthNumberRepository;
+    // 메일 전송
+    private final MailProvider mailProvider;
+    // // Jwt키 생성 및 검증
+    // private final JwtProvider jwtProvider;
+    // 비밀번호 암호화
     private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     @Override
@@ -62,10 +78,26 @@ public class AuthServiceImplement implements AuthService{
     public ResponseEntity<ResponseDto> emailAuth(EmailAuthRequestDto dto) {
 
         try{
+
             String userEmail = dto.getUserEmail();
+
             boolean existEmail = userRepository.existsByUserEmail(userEmail);
-            if(existEmail) return ResponseDto.existUser();
+            // 이메일 중복 에러 코드 전송
+            if(existEmail) return ResponseDto.duplicatiedEmail();
+
+            // 생성한 인증번호 를 authNumber에 저장
+            String authNumber = EmailAuthNumberUtil.createNumber();
+
+            EmailAuthEntity emailAuthEntity = new EmailAuthEntity(userEmail, authNumber);
+            // 저장소에 이메일과, 인증번호 저장
+            emailAuthNumberRepository.save(emailAuthEntity);
+
+            // 메일 전송 기능 사용
+            mailProvider.mailAuthSend(userEmail, authNumber);
   
+          } catch(MessagingException exception){ 
+            exception.printStackTrace();
+            return ResponseDto.mailSendFail();
           } catch(Exception exception){ 
               exception.printStackTrace();
               return ResponseDto.databaseError();
@@ -77,8 +109,21 @@ public class AuthServiceImplement implements AuthService{
 
     @Override
     public ResponseEntity<ResponseDto> emailAuthCheck(EmailAuthCheckRequestDto dto) {
-        // TODO Auto-generated method stub  
-        throw new UnsupportedOperationException("Unimplemented method 'emailAuthCheck'");
+
+        try { 
+
+            String userEmail = dto.getUserEmail();
+            String authNumber = dto.getAuthNumber();
+
+            // 이메일과 인증번호 확인
+            boolean isMatched = emailAuthNumberRepository.existsByEmailAndAuthNumber(userEmail, authNumber);
+            // 만일 이메일과 인증번호가 일치하지 않는다면 'AF' 인증실패
+            if (!isMatched) return ResponseDto.authFail();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+        return ResponseDto.success();
     }
 
     @Override
