ac72713508ccfe465453e0766100756dc343c6f7
young <lovetkddud@naver.com>
2025-04-16 16:32:29 +0900

    psy

---
+++ src/main/java/com/ateam/jjimppong_back/repository/PopupStoreRepository.java
@@ -0,0 +1,11 @@
+package com.ateam.jjimppong_back.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ateam.jjimppong_back.common.entity.PopupStoreEntity;
+
+public interface PopupStoreRepository extends JpaRepository<PopupStoreEntity, Integer> {
+    List<PopupStoreEntity> findByRegion(String region);
+}
