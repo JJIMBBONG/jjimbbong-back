5c1eedeb9355ccffb1456eab0422f9cd819636c5
young <lovetkddud@naver.com>
2025-05-14 17:26:21 +0900

    WebConfig.java: allowedOriginPatterns= http://13.124.81.10:3000 추가

--- src/main/java/com/ateam/jjimppong_back/config/WebConfig.java
+++ src/main/java/com/ateam/jjimppong_back/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("http://localhost:3000")
+                        .allowedOriginPatterns("http://localhost:3000", "http://13.124.81.10:3000")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
